fafa10d70656209155dda58cf5ac37fcdbd524fd
ReidoBoss <bryansagarino222@gmail.com>
2025-09-09 15:50:26 +0800

    refactor: edit RestauarantParamsSchema's property to optional
    all RestaurantParamsSchema ar now optional instead of nullish to fix the
    type errors

--- src/schema/restaurant-finder/restaurant-params.schema.ts
+++ src/schema/restaurant-finder/restaurant-params.schema.ts
@@ -4,14 +4,14 @@ import { TelFormatSchema } from './tel-format.schema'
 import { SortSchema } from './sort.schema'
 
 export const RestaurantParamsSchema = z.object({
-  query: StringSchema.nullish(),
-  radius: NumberSchema.max(100000).nullish(),
-  min_price: NumberSchema.min(1).max(4).nullish(),
-  max_price: NumberSchema.min(1).max(4).nullish(),
-  open_at: StringSchema.nullish(),
-  open_now: BooleanSchema.nullish(),
-  tel_format: TelFormatSchema.nullish(),
-  near: StringSchema.nullish(),
-  sort: SortSchema.nullish(),
-  limit: NumberSchema.min(1).max(4).nullish(),
+  query: StringSchema.optional(),
+  radius: NumberSchema.max(100000).optional(),
+  min_price: NumberSchema.min(1).max(4).optional(),
+  max_price: NumberSchema.min(1).max(4).optional(),
+  open_at: StringSchema.optional(),
+  open_now: BooleanSchema.optional(),
+  tel_format: TelFormatSchema.optional(),
+  near: StringSchema.optional(),
+  sort: SortSchema.optional(),
+  limit: NumberSchema.min(1).max(4).optional(),
 })
