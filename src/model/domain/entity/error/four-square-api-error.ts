417dfdf064fdf19b8d6cb4543fa3381726c04afa
ReidoBoss <bryansagarino222@gmail.com>
2025-09-09 15:48:36 +0800

    chore: create error classes for four place ai

---
+++ src/model/domain/entity/error/four-square-api-error.ts
@@ -0,0 +1,4 @@
+export class FourplaceError extends Error {}
+export class FourPlaceApiBadRequestError extends FourplaceError {}
+export class FourPlaceApiUnauthorizedError extends FourplaceError {}
+export class FourPlaceApiJsonParsingError extends FourplaceError {}
